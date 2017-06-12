59607c43d5974a83524b33e199b082005f4105d4
iSelly <xiao7shuang@qq.com>
2017-06-12 16:52:01 +0800

    导出功能优化

--- Application/Admin/Controller/MainController.class.php
+++ Application/Admin/Controller/MainController.class.php
@@ -6,6 +6,7 @@ use Think\Controller;
 
 class MainController extends AdminController {
 
+    /**提取起止日期**/
     private function _queryTime(){
         $start_time= strtotime(I('start_time'));
         $end_time= strtotime(I('end_time'));
@@ -16,7 +17,6 @@ class MainController extends AdminController {
                 $map['create_time'] = array(array('gt', $start_time), array('lt', $end_time));
             }
         }
-        $map['status']  =   array('egt',0);
         return $map;
     }
 
@@ -98,27 +98,26 @@ class MainController extends AdminController {
         if (IS_POST) {
             $map = $this -> _queryTime();
             $staff_name = I('staff_name');
-            if(is_numeric($staff_name)){
-                $map["id|staff_name"] =   array(intval($staff_name),array('like','%'.$staff_name.'%'),'_multi'=>true);
-            }else{
-                $map['staff_name']    =   array('like', '%'.(string)$staff_name.'%');
+            if($staff_name) {
+                if (is_numeric($staff_name)) {
+                    $map["id|staff_name"] = array(intval($staff_name), array('like', '%' . $staff_name . '%'), '_multi' => true);
+                } else {
+                    $map['staff_name'] = array('like', '%' . (string)$staff_name . '%');
+                }
             }
             $end_time = $map['create_time'];
             if(empty($end_time) && empty($staff_name)){
                 $xlsName = 'Staff全表导出';
-                $xlsData = $xlsModel->Field($field)->order('id DESC')->select();
             }elseif(empty($end_time) && $staff_name){
                 $xlsName = 'Staff表专员搜索结果导出';
-                $where['id|staff_name'] = array(intval($staff_name), array('like', '%' . $staff_name . '%'), '_multi' => true);
-                $xlsData = $xlsModel->Field($field)->where($where)->order('id DESC')->select();
             }else {
                 $xlsName = 'Staff表搜索结果导出';
-                $xlsData = $xlsModel->Field($field)->where($map)->order('id DESC')->select();
             }
+            $xlsData = $xlsModel->Field($field)->where($map)->order('id DESC')->select();
         }
         foreach ($xlsData as $k => $v) {
             $xlsData[$k]['create_time'] = $v['create_time'] == 0 ? '-' : date("Y-m-d H:i",$v['create_time']);
-            $xlsData[$k]['status'] = $v['status'] == 1 ? '正常' : '禁用';
+            $xlsData[$k]['status']      = $v['status']      == 1 ? '正常' : '禁用';
         }
         $excel->exportExcel($xlsName,$xlsCell,$xlsData);
     }
@@ -224,21 +223,14 @@ class MainController extends AdminController {
     /**任务列表**/
     public function taskList(){
         $dbTask = M('Task');
+        $map = $this -> _queryTime();
         $task_name = I('task_name');
-        $start = strtotime(I('start_time'));
-        $end = strtotime(I('end_time'));
-        if($start || $end){
-            if($start >= $end){
-                $this -> error('查询的开始日期大于结束日期，这让我很为难啊...');
-            }else{
-                $map['create_time'] = array(array('gt', $start), array('lt', $end));
-            }
-        }
         if(is_numeric($task_name)){
             $map['id|name'] =   array(intval($task_name),array('like','%'.$task_name.'%'),'_multi'=>true);
         }else{
             $map['name']    =   array('like', '%'.(string)$task_name.'%');
         }
+        //p($map);
         $resTask = $this -> lists($dbTask,$map);
         int_to_string($resTask);
         $this -> assign('resTask',$resTask);
@@ -273,36 +265,36 @@ class MainController extends AdminController {
         if (IS_POST) {
             $map = $this -> _queryTime();
             $task_name = I('task_name');
-            if(is_numeric($task_name)){
-                $map["id|name"] =   array(intval($task_name),array('like','%'.$task_name.'%'),'_multi'=>true);
-            }else{
-                $map['name']    =   array('like', '%'.(string)$task_name.'%');
+            if($task_name) {
+                if (is_numeric($task_name)) {
+                    $map["id|name"] = array(intval($task_name), array('like', '%' . $task_name . '%'), '_multi' => true);
+                } else {
+                    $map['name'] = array('like', '%' . (string)$task_name . '%');
+                }
             }
             $end_time = $map['create_time'];
             if(empty($end_time) && empty($task_name)){
                 $xlsName = 'Task全表导出';
-                $xlsData = $xlsModel->Field($field)->order('id DESC')->select();
             }elseif(empty($end_time) && $task_name){
                 $xlsName = 'Task表专员搜索结果导出';
-                $where["id|name"] =   array(intval($task_name),array('like','%'.$task_name.'%'),'_multi'=>true);
-                $xlsData = $xlsModel->Field($field)->where($where)->order('id DESC')->select();
             }else {
                 $xlsName = 'Task表搜索结果导出';
-                $xlsData = $xlsModel->Field($field)->where($map)->order('id DESC')->select();
             }
+            $xlsData = $xlsModel->Field($field)->where($map)->order('id DESC')->select();
         }
         $status = array('未发布','进行中','已过期');
         $type = array('-','基本任务','额外任务');
         foreach ($xlsData as $k => $v) {
-            $xlsData[$k]['start_time'] = $v['start_time'] == 0 ? '-' : date("Y-m-d H:i",$v['start_time']);
-            $xlsData[$k]['end_time'] = $v['end_time'] == 0 ? '-' : date("Y-m-d H:i",$v['end_time']);
+            $xlsData[$k]['start_time']  = $v['start_time']  == 0 ? '-' : date("Y-m-d H:i",$v['start_time']);
+            $xlsData[$k]['end_time']    = $v['end_time']    == 0 ? '-' : date("Y-m-d H:i",$v['end_time']);
             $xlsData[$k]['create_time'] = $v['create_time'] == 0 ? '-' : date("Y-m-d H:i",$v['create_time']);
-            $xlsData[$k]['tasker'] = $v['tasker'] == '' ? '-' : $v['tasker'];
-            $xlsData[$k]['status'] = $status[$v['status']];
-            $xlsData[$k]['type'] = $type[$v['type']];
+            $xlsData[$k]['tasker']      = $v['tasker']      =='' ? '-' : $v['tasker'];
+            $xlsData[$k]['status']      = $status[$v['status']];
+            $xlsData[$k]['type']        = $type[$v['type']];
         }
         $excel->exportExcel($xlsName,$xlsCell,$xlsData);
     }
+
 
     /**添加任务**/
     public function taskAdd(){
@@ -332,17 +324,9 @@ class MainController extends AdminController {
     /**任务发布记录**/
     public function taskPost(){
         $dbTask = M('Task');
+        $map = $this -> _queryTime();
         $task_name = I('task_name');
-        $start = strtotime(I('start_time'));
-        $end = strtotime(I('end_time'));
-        if($start || $end){
-            if($start >= $end){
-                $this -> error('查询的开始日期大于结束日期，这让我很为难啊...');
-            }else{
-                $map['create_time'] = array(array('gt', $start), array('lt', $end));
-            }
-        }
-        $map['status']  =   array('eq',1);
+        $map['status']  = 1;
         if(is_numeric($task_name)){
             $map['id|name'] =   array(intval($task_name),array('like','%'.$task_name.'%'),'_multi'=>true);
         }else{
@@ -353,6 +337,65 @@ class MainController extends AdminController {
         $this -> assign('resTask',$resTask);
         $this -> meta_title = '任务发布记录';
         $this -> display('Main/task/taskPost');
+    }
+
+
+    /**任务发布记录-导出**/
+    public function taskPostOut(){
+        $excel = A('Excel');
+        $xlsCell = array(
+            array('id', 'ID'),
+            array('name', '任务名称'),
+            array('type', '任务类型'),
+            array('inneed', '任务指标'),
+            array('start_time', '开始时间'),
+            array('end_time', '结束时间'),
+            array('create_time', '注册时间'),
+            array('tasker', '发布者'),
+            array('status', '状态'),
+        );
+        $field = null;
+        foreach ($xlsCell as $key => $value) {
+            if($key == 0){
+                $field = $value[0];
+            }else{
+                $field .= "," . $value[0];
+            }
+        }
+        $xlsModel = M('Task');
+        if (IS_POST) {
+            $map = $this -> _queryTime();
+            $map['status'] = 1;
+            $task_name = I('task_name');
+            if($task_name) {
+                if (is_numeric($task_name)) {
+                    $map["id|name"] = array(intval($task_name), array('like', '%' . $task_name . '%'), '_multi' => true);
+                } else {
+                    $map['name'] = array('like', '%' . (string)$task_name . '%');
+                }
+            }
+            $end_time = $map['create_time'];
+            if(empty($end_time) && empty($task_name)){
+                $xlsName = 'Task全表导出';
+            }elseif(empty($end_time) && $task_name){
+                $xlsName = 'Task表专员搜索结果导出';
+            }else {
+                $xlsName = 'Task表搜索结果导出';
+            }
+            $xlsData = $xlsModel->Field($field)->where($map)->order('id DESC')->select();
+        }
+        $type = array('-','基本任务','额外任务');
+        foreach ($xlsData as $k => $v) {
+            $xlsData[$k]['start_time']    = $v['start_time']  == 0 ? '-' : date("Y-m-d H:i",$v['start_time']);
+            $xlsData[$k]['end_time']      = $v['end_time']    == 0 ? '-' : date("Y-m-d H:i",$v['end_time']);
+            $xlsData[$k]['create_time']   = $v['create_time'] == 0 ? '-' : date("Y-m-d H:i",$v['create_time']);
+            $xlsData[$k]['tasker']        = $v['tasker']      =='' ? '-' : $v['tasker'];
+            //$xlsData[$k]['status']        = [$v['status']]    == 1 ? '进行中' :'-' ;
+            $xlsData[$k]['type']          = $type[$v['type']];
+        }
+        echo $xlsModel -> _sql();
+        p($map);p($xlsData);die;
+        $excel->exportExcel($xlsName,$xlsCell,$xlsData);
     }
 
 
