8108723bec8e062ebbe81f6a6f5d101e8975b4eb
iSelly <xiao7shuang@qq.com>
2017-06-15 14:19:26 +0800

    修复更换根目录后丢失的文件

--- Application/Admin/Controller/MainController.class.php
+++ Application/Admin/Controller/MainController.class.php
@@ -26,206 +26,12 @@ class MainController extends AdminController {
     }
 
 
-    /**消息管理**/
-    public function notice($method = null){
-        $dbNotice = D('notice');
-        $map = $this -> _queryTime();
-        $notice_name = I('notice_name');
-        $notice_type = I('notice_type');
-        if($notice_name) {
-            if (is_numeric($notice_name)) {
-                $map['id'] = array(intval($notice_name), array('EQ', $notice_name));
-            } else {
-                $map['notice_title'] = array('like', '%' . $notice_name . '%');
-            }
-        }
-        if($notice_type){
-            $map['notice_type_id'] = $notice_type;
-        }
-        $resNotice = $this -> lists($dbNotice,$map);
-        int_to_string($resNotice);
-        foreach ($resNotice as $key=> $value )
-        {
-            $noticeData[$key]['id'] = $value['id'];
-            $noticeData[$key]['create_time'] = $value['create_time'];
-            $noticeData[$key]['create_ip'] = $value['create_ip'];
-            $noticeData[$key]['notice_title'] = $value['notice_title'];
-            $noticeData[$key]['notice_content'] = $value['notice_content'];
-            $noticeData[$key]['img_url'] = $value['img_url'];
-            $noticeType = M("notice_type");
-            $notice_type_id = $value['notice_type_id'];
-            $noticeData[$key]['type'] = $noticeType->where(array('id'=>$notice_type_id))->find();
-            if(strlen($resNotice[$key]['notice_content']) < 80){
-                $noticeData[$key]['n_content'] = $resNotice[$key]['notice_content'];
-        }else{
-                $noticeData[$key]['n_content'] = msubstr($resNotice[$key]['notice_content'],0,30);
-        }
-        }
-        $result = D('notice_type')->select();
-        $this->assign('type',$result);
-        $this->assign('notice',$noticeData);
-        //导出查询到的数据（不含分页）
-        $excel = A('Excel');
-        $xlsCell = array(
-            array('id', 'ID'),
-            array('notice_title', '公告主题'),
-            array('create_ip', 'IP'),
-            array('notice_content', '功能内容'),
-            array('notice_type_id', '公告类型'),
-            array('img_url', '图片地址'),
-            array('create_time', '创建时间'),
-        );
-<<<<<<< HEAD
-        /*
-=======
->>>>>>> 196a1625e6c7a1151f01c9a92fbddf24757fa049
-        $field = null;
-        foreach ($xlsCell as $key => $value) {
-            if($key == 0){
-                $field = $value[0];
-            }else{
-                $field .= "," . $value[0];
-            }
-        }
-<<<<<<< HEAD
-        */
-=======
->>>>>>> 196a1625e6c7a1151f01c9a92fbddf24757fa049
-        $end_time = $map['create_time'];
-        if(empty($end_time) && empty($staff_name)){
-            $xlsName = 'Notice全表导出';
-        }elseif(empty($end_time) && $staff_name){
-            $xlsName = 'Notice表通知主题搜索结果导出';
-        }else {
-            $xlsName = 'Notice表搜索结果导出';
-        }
-<<<<<<< HEAD
-//        $xlsData = $dbNotice->Field($field)->where($map)->order('id DESC')->select();
-        $xlsData = $dbNotice->where($map)->order('id DESC')->select();
-=======
-        $xlsData = $dbNotice->Field($field)->where($map)->order('id DESC')->select();
->>>>>>> 196a1625e6c7a1151f01c9a92fbddf24757fa049
-        foreach ($xlsData as $k => $v) {
-            $xlsData[$k]['create_time'] = $v['create_time'] == 0 ? '-' : date("Y-m-d H:i",$v['create_time']);
-            $xlsData[$k]['status']      = $v['status']      == 1 ? '正常' : '禁用';
-        }
-        switch (strtolower($method)){
-            case 'list':
-                //nothing need to do here!
-                break;
-            case 'out':
-                $excel->exportExcel($xlsName,$xlsCell,$xlsData);
-                break;
-        }
-
-        $this -> meta_title = '消息管理';
-        $this -> display('Main/notice/notice');
-    }
-
-    /**发布消息**/
-    public function noticePost(){
-        if($_POST)
-        {
-            //提交公告内容信息
-            if($_POST['notice_type']>0&&$_POST['notice_title']&&$_POST['content']){
-                $notice = D('notice');
-                $maxid = $notice->max('id');
-                $data['id'] = $maxid+1;
-                $data['create_ip'] = $_SERVER['REMOTE_ADDR'];
-                $data['notice_content'] = $_POST['content'];
-                $data['notice_type_id'] = $_POST['notice_type'];
-                $data['notice_title'] = $_POST['notice_title'];
-                //上传图片信息
-                $upload = new \Think\Upload();
-                $upload->maxSize   =     2*1024*1024 ;// 设置附件上传大小
-                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-                $upload->rootPath  =     './Uploads/Picture/'; // 设置附件上传根目录
-                $info   =   $upload->uploadOne($_FILES['photo1']);//上传文件信息
-                $savepath = $info['savepath'];//存放图片的文件夹名
-                $savename = $info['savename'];//图片名
-                $img_url = 'http://'.$_SERVER['HTTP_HOST'].'/Uploads/Picture/'.$savepath.$savename;//拼接图片地址
-                $data['img_url'] = $img_url;//存放图片路径
-                if($info&&$notice->add($data)){
-                    $this -> success('提交成功',U('Main/notice'));
-                }
-                else{
-                    $this -> error('提交失败');
-                }
-            }
-            else{
-                echo "<script>alert('请正确输入信息');history.go(-1); </script>";
-            }
-        }
-        $result = D('notice_type')->select();
-        $this->assign('type',$result);
-        $this -> meta_title = '消息管理';
-        $this -> display('Main/notice/noticePost');
-    }
-
-    /**修改消息**/
-    public function noticeEdit()
-    {
-        $dbNotice = M('notice');
-        $dbNoticeType = M('notice_type');
-        $noticeID = I('id');
-        $resNotice = $dbNotice -> where(array('id='.$noticeID))->find();
-        $resNoticeType = $dbNoticeType -> where(array('id='. $resNotice['notice_type_id']))->find();//$resNoticeType['notice_type_name']消息类型
-        $resNoticeAllType = $dbNoticeType -> select();
-        if(IS_POST)
-        {
-            header('Content-Type:text/html;charset=utf-8');
-            $noticeTypeName = $_POST['noticeType'];
-            $resNoticeTypeData = $dbNoticeType -> where(array('notice_type_name' => $noticeTypeName))->find();
-            $resNoticeTypeID = $resNoticeTypeData['id'];
-            $data = array(
-                'id' => I('post.noticeID'), //消息id
-                'notice_title' => I('post.noticeTitle'),//消息标题
-                'notice_content'=> I('post.noticeContent'),//消息内容
-            );
-            $data['create_time'] = time();
-            $data['create_ip'] = $_SERVER['REMOTE_ADDR'];
-            $data['notice_type_id'] = $resNoticeTypeID;
-            //图片信息
-            $upload = new \Think\Upload();
-            $upload->maxSize   =     2*1024*1024 ;// 设置附件上传大小
-            $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-            $upload->rootPath  =     './Uploads/Picture/'; // 设置附件上传根目录
-            $info   =   $upload->uploadOne($_FILES['photo']);//上传文件信息
-            //$info   =   $upload->uploadOne($_FILES['photo']);//上传文件信息
-            $savepath = $info['savepath'];//存放图片的文件夹名
-            $savename = $info['savename'];//图片名
-            $img_url = 'http://'.$_SERVER['HTTP_HOST'].'/Uploads/Picture/'.$savepath.$savename;//拼接图片地址
-            $data['img_url'] = $img_url;//存放图片路径
-            if($dbNotice->where(array('id='.$data['id']))->save($data)){
-                $this -> success('修改成功',U('Main/notice'));
-            }else{
-                $this -> error('修改失败',U('Main/notice'));
-            }
-        }
-        $this -> assign('noticeID',$resNotice);
-        $this -> assign('noticeTitle',$resNotice);
-        $this -> assign('noticeContent',$resNotice);
-        $this -> assign('noticeType',$resNoticeType);
-        $this -> assign('noticeAllType', $resNoticeAllType);
-        $this -> meta_title = '修改消息信息';
-        $this -> display('Main/Notice/noticeEdit');
-    }
-
-    /**删除消息**/
-    public function noticeDelete()
-    {
-        $notice = D('notice');
-        $noticeDeleteId = I('id');//获取删除的ID
-        $notice->where('id='.$noticeDeleteId)->delete(); // 删除id为5的用户数据
-        $this -> success('删除成功',U('Main/notice'));
-    }
-
-
     /**推广专员信息管理及导出**/
     public function msgList($method = null){
         $dbStaff = M('Staff');
         $map = $this -> _queryTime();
         $staff_name = I('staff_name');
+        $map['status'] = array('egt',0);
         if($staff_name) {
             if (is_numeric($staff_name)) {
                 $map["id|staff_name"] = array(intval($staff_name), array('like', '%' . $staff_name . '%'), '_multi' => true);
@@ -290,14 +96,15 @@ class MainController extends AdminController {
         $dbStaff = M('staff');
         $where['id'] = I('id');
         if(IS_POST){
+            $id['id'] = $_POST['id'];
             $data = array(
-                'staff_name' => I('post.name'),
-                'referee' => I('post.referee'),
-                'mobile' => I('post.mobile'),
+                'staff_name' => I('name'),
+                'referee' => I('referee'),
+                'mobile' => I('mobile'),
             );
-            $resStaff = $dbStaff -> where($where) -> save($data);
+            $resStaff = $dbStaff -> where($id) -> save($data);
             if($resStaff == 0){
-                $this -> success('什么都没更改，正跳转至列表页...',U('Main/msgList'));
+                $this -> success('什么都没修改，跳转至列表页',U('Main/msgList'));
             }elseif($resStaff == 1){
                 $this -> success('修改成功',U('Main/msgList'));
             }else{
@@ -373,9 +180,10 @@ class MainController extends AdminController {
         if($status) {
             $map['status'] = $status;
         }
-        //The following code can't be moved to other palce!
+        //Begin >>The following code can't be moved to other palce!
         $this -> assign('type',$type);
         $this -> assign('status',$status);
+        //End<<
         if($task_name) {
             if (is_numeric($task_name)) {
                 $map['id|name'] = array(intval($task_name), array('like', '%' . $task_name . '%'), '_multi' => true);
@@ -406,9 +214,9 @@ class MainController extends AdminController {
             }
         }
         $end_time = $map['create_time'];
-        if(empty($end_time) && empty($staff_name)){
+        if(empty($end_time) && empty($task_name) && empty($type) && empty($status)){
             $xlsName = 'Task全表导出';
-        }elseif(empty($end_time) && $staff_name){
+        }elseif($task_name){
             $xlsName = 'Task表专员搜索结果导出';
         }else {
             $xlsName = 'Task表搜索结果导出';
@@ -494,6 +302,7 @@ class MainController extends AdminController {
         $dbTask = M('Task');
         $where['id'] = I('id');
         if(IS_POST){
+            $id['id'] = $_POST['id'];
             $data = array(
                 'name' => I('name', '', 'htmlspecialchars'),
                 'type' => I('post.type'),
@@ -503,7 +312,7 @@ class MainController extends AdminController {
                 'tasker' => session('user_auth')['username'],
                 'status' => I('status'),
             );
-            $resStaff = $dbTask -> where($where) -> save($data);
+            $resStaff = $dbTask -> where($id) -> data($data) -> save();
             if($resStaff == 0){
                 $this -> success('什么都没更改，正跳转至列表页...',U('Main/taskList'));
             }elseif($resStaff == 1){
@@ -528,6 +337,184 @@ class MainController extends AdminController {
         $this->assign('resTask', $resTask);
         $this->meta_title = '编辑任务';
         $this->display('Main/task/taskView');
+    }
+
+
+    /**消息管理**/
+    public function notice($method = null){
+        $dbNotice = M('notice');
+        $noticeType = M('notice_type');
+        $map = $this -> _queryTime();
+        $notice_name = I('notice_name');
+        $notice_type = I('notice_type');
+        $this -> assign('notice_type',$notice_type);
+        if($notice_name) {
+            if (is_numeric($notice_name)) {
+                $map['id|notice_title'] = array(intval($notice_name), array('EQ', $notice_name));
+            } else {
+                $map['notice_title'] = array('like', '%' . $notice_name . '%');
+            }
+        }
+        if($notice_type){
+            $map['notice_type_id'] = $notice_type;
+        }
+        $resNotice = $this -> lists($dbNotice,$map);
+        foreach ($resNotice as $k => $v) {
+            if(strlen($resNotice[$k]['notice_content']) < 100){
+                $resNotice[$k]['n_content'] = $resNotice[$k]['notice_content'];
+            }else{
+                $resNotice[$k]['n_content'] = msubstr($resNotice[$k]['notice_content'],0,80);
+            }
+            $id['id'] =$resNotice[$k]['notice_type_id'];
+            $type = $noticeType -> where($id) -> find();
+            $resNotice[$k]['notice_type_name'] = $type['notice_type_name'];
+        }
+        $type = $noticeType -> select();
+        $this -> assign('type',$type);
+        $this -> assign('resNotice',$resNotice);
+        //导出查询到的数据（不含分页）
+        $excel = A('Excel');
+        $xlsCell = array(
+            array('id', 'ID'),
+            array('notice_title', '公告主题'),
+            array('create_ip', 'IP'),
+            array('notice_content', '功能内容'),
+            array('notice_type_id', '公告类型'),
+            array('img_url', '图片地址'),
+            array('create_time', '创建时间'),
+        );
+        $field = null;
+        foreach ($xlsCell as $key => $value) {
+            if($key == 0){
+                $field = $value[0];
+            }else{
+                $field .= "," . $value[0];
+            }
+        }
+        $end_time = $map['create_time'];
+        if(empty($end_time) && empty($notice_name) && empty($notice_type)){
+            $xlsName = 'Notice全表导出';
+        }elseif($notice_name){
+            $xlsName = 'Notice表通知主题搜索结果导出';
+        }else {
+            $xlsName = 'Notice表搜索结果导出';
+        }
+        $xlsData = $dbNotice->Field($field)->where($map)->order('id DESC')->select();
+        foreach ($xlsData as $k => $v) {
+            $xlsData[$k]['create_time'] = $v['create_time'] == 0 ? '-' : date("Y-m-d H:i",$v['create_time']);
+            $xlsData[$k]['status']      = $v['status']      == 1 ? '正常' : '禁用';
+        }
+        switch (strtolower($method)){
+            case 'list':
+                //nothing need to do here!
+                break;
+            case 'out':
+                $excel->exportExcel($xlsName,$xlsCell,$xlsData);
+                break;
+        }
+        $this -> meta_title = '消息管理';
+        $this -> display('Main/notice/notice');
+    }
+
+
+    /**发布消息**/
+    public function noticePost(){
+        if($_POST)
+        {
+            //提交公告内容信息
+            if($_POST['notice_type']>0&&$_POST['notice_title']&&$_POST['content']){
+                $notice = D('notice');
+                $data['create_ip'] = $_SERVER['REMOTE_ADDR'];
+                $data['aid'] = session('user_auth')['username'];
+                $data['notice_content'] = $_POST['content'];
+                $data['notice_type_id'] = $_POST['notice_type'];
+                $data['notice_title'] = $_POST['notice_title'];
+                //上传图片信息
+                $upload = new \Think\Upload();
+                $upload->maxSize   =     2*1024*1024 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =     './Uploads/Picture/'; // 设置附件上传根目录
+                $info   =   $upload->uploadOne($_FILES['photo1']);//上传文件信息
+                $savepath = $info['savepath'];//存放图片的文件夹名
+                $savename = $info['savename'];//图片名
+                $img_url = 'http://'.$_SERVER['HTTP_HOST'].'/Uploads/Picture/'.$savepath.$savename;//拼接图片地址
+                $data['img_url'] = $img_url;//存放图片路径
+                if($info&&$notice->add($data)){
+                    $this -> success('提交成功',U('Main/notice'));
+                }elseif(empty($info)){
+                    $this -> error('请选择图片再提交');
+                }else{
+                    $this -> error('提交失败');
+                }
+            }
+            else{
+                echo "<script>alert('请正确输入信息');history.go(-1); </script>";
+            }
+        }else {
+            $result = D('notice_type')->select();
+            $this->assign('type', $result);
+            $this->meta_title = '消息管理';
+            $this->display('Main/notice/noticePost');
+        }
+    }
+
+
+    /**修改消息**/
+    public function noticeEdit()
+    {
+        $dbNotice = M('notice');
+        $dbNoticeType = M('notice_type');
+        $noticeID = I('id');
+        $resNotice = $dbNotice -> where(array('id='.$noticeID))->find();
+        $resNoticeType = $dbNoticeType -> where(array('id='. $resNotice['notice_type_id']))->find();//$resNoticeType['notice_type_name']消息类型
+        $resNoticeAllType = $dbNoticeType -> select();
+        if(IS_POST)
+        {
+            header('Content-Type:text/html;charset=utf-8');
+            $noticeTypeName = $_POST['noticeType'];
+            $resNoticeTypeData = $dbNoticeType -> where(array('notice_type_name' => $noticeTypeName))->find();
+            $resNoticeTypeID = $resNoticeTypeData['id'];
+            $data = array(
+                'id' => I('post.noticeID'), //消息id
+                'notice_title' => I('post.noticeTitle'),//消息标题
+                'notice_content'=> I('post.noticeContent'),//消息内容
+            );
+            $data['create_time'] = time();
+            $data['create_ip'] = $_SERVER['REMOTE_ADDR'];
+            $data['notice_type_id'] = $resNoticeTypeID;
+            //图片信息
+            $upload = new \Think\Upload();
+            $upload->maxSize   =     2*1024*1024 ;// 设置附件上传大小
+            $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+            $upload->rootPath  =     './Uploads/Picture/'; // 设置附件上传根目录
+            $info   =   $upload->uploadOne($_FILES['photo']);//上传文件信息
+            //$info   =   $upload->uploadOne($_FILES['photo']);//上传文件信息
+            $savepath = $info['savepath'];//存放图片的文件夹名
+            $savename = $info['savename'];//图片名
+            $img_url = 'http://'.$_SERVER['HTTP_HOST'].'/Uploads/Picture/'.$savepath.$savename;//拼接图片地址
+            $data['img_url'] = $img_url;//存放图片路径
+            if($dbNotice->where(array('id='.$data['id']))->save($data)){
+                $this -> success('修改成功',U('Main/notice'));
+            }else{
+                $this -> error('修改失败',U('Main/notice'));
+            }
+        }
+        $this -> assign('noticeID',$resNotice);
+        $this -> assign('noticeTitle',$resNotice);
+        $this -> assign('noticeContent',$resNotice);
+        $this -> assign('noticeType',$resNoticeType);
+        $this -> assign('noticeAllType', $resNoticeAllType);
+        $this -> meta_title = '修改消息信息';
+        $this -> display('Main/Notice/noticeEdit');
+    }
+
+    /**删除消息**/
+    public function noticeDelete()
+    {
+        $notice = D('notice');
+        $noticeDeleteId = I('id');//获取删除的ID
+        $notice->where('id='.$noticeDeleteId)->delete(); // 删除id为5的用户数据
+        $this -> success('删除成功',U('Main/notice'));
     }
 
 
