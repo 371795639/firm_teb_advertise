96fa7c77c437afc211f1163ced3cf69f6fa01151
iSelly <xiao7shuang@qq.com>
2017-07-15 09:37:51 +0800

    分享推广专员任务逻辑重写完成。

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -55,9 +55,13 @@ class UserController extends HomeController {
                                 if ($gameId){
                                     echo "<script>alert('此游戏ID已被占用，请检查输入');</script>";
                                 }else{
+                                    //更新用户信息
                                     $refData['referee'] = $refStaffExist['id'];
                                     $ref = $dbStaff->where('id=' . $userid)->save($refData);
-                                    if($ref){
+                                    //给推荐人recommend_num+1
+                                    $refDateRecommend['recommend_num'] = $refStaffExist['recommend_num'] + 1;
+                                    $refAdd = $dbStaff ->  save_staff_by_id($refStaffExist['id'],$refDateRecommend);
+                                    if($ref && $refAdd){
                                         $this->redirect('User/index');
                                     }
                                 }
